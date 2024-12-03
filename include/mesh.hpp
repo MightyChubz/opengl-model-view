20a7207216d1a76ffa1313999f3a6514835ca9bb
Alexander Cusaac <theradcast16@gmail.com>
2024-12-02 22:56:58 -0500

    feat: Add mesh for mesh object handling and rendering

---
+++ include/mesh.hpp
@@ -0,0 +1,33 @@
+#ifndef MESH_HPP
+#define MESH_HPP
+
+#include <GL/glew.h>
+#include <cstddef>
+#include <memory>
+#include <string_view>
+
+#include "stddefs.hpp"
+#include "texture.hpp"
+
+class Mesh
+{
+    std::shared_ptr<u32> vao;
+    std::shared_ptr<u32> vbo;
+    std::shared_ptr<u32> ebo;
+    size_t               indice_size;
+
+    static void          destruct_attribute_array(u32 *vao)
+    {
+        glDeleteVertexArrays(1, vao);
+    }
+    static void destruct_buffer_array(u32 *bo)
+    {
+        glDeleteBuffers(1, bo);
+    }
+
+  public:
+    Mesh(const std::string_view path);
+    void render();
+};
+
+#endif
---
+++ src/mesh.cpp
@@ -0,0 +1,51 @@
+#include "mesh.hpp"
+#include "glm/ext/matrix_float4x4.hpp"
+#include "mesh_loader.hpp"
+#include <string_view>
+
+Mesh::Mesh(const std::string_view path)
+{
+    MeshLoader           loader;
+    MeshLoader::MeshData data = loader.load_obj(path);
+
+    u32                  vao, vbo, ebo;
+    glGenVertexArrays(1, &vao);
+    glGenBuffers(1, &vbo);
+    glGenBuffers(1, &ebo);
+
+    glBindVertexArray(vao);
+
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+    glBufferData(GL_ARRAY_BUFFER, data.vertices.size() * sizeof(Vertex), data.vertices.data(), GL_STATIC_DRAW);
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, data.indices.size() * sizeof(u32), data.indices.data(), GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), static_cast<void *>(0));
+    glEnableVertexAttribArray(0);
+
+    glVertexAttribPointer(1,
+                          2,
+                          GL_FLOAT,
+                          GL_FALSE,
+                          sizeof(Vertex),
+                          reinterpret_cast<void *>(offsetof(Vertex, texcoord)));
+    glEnableVertexAttribArray(1);
+
+    glBindVertexArray(0);
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+    this->vao.reset(new u32(vao), destruct_attribute_array);
+    this->vbo.reset(new u32(vbo), destruct_buffer_array);
+    this->ebo.reset(new u32(ebo), destruct_buffer_array);
+    indice_size = data.indices.size();
+}
+
+void Mesh::render()
+{
+    glBindVertexArray(*vao);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, *ebo);
+    glDrawElements(GL_TRIANGLES, indice_size, GL_UNSIGNED_INT, 0);
+    glBindVertexArray(0);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+}
