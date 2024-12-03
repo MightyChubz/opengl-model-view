3bb1d9c6806384808b961d430cc4b1ae9476dcaf
Alexander Cusaac <theradcast16@gmail.com>
2024-12-03 06:33:55 -0500

    feat: Replace camera logic in main with Camera class

--- src/main.cpp
+++ src/main.cpp
@@ -15,13 +15,12 @@
 #include "SDL_timer.h"
 #include "SDL_video.h"
 #include "asset_registry.hpp"
-#include "glm/ext/matrix_clip_space.hpp"
+#include "camera.hpp"
 #include "glm/ext/matrix_float4x4.hpp"
 #include "glm/ext/matrix_transform.hpp"
 #include "glm/ext/vector_float2.hpp"
 #include "glm/ext/vector_float3.hpp"
 #include "glm/fwd.hpp"
-#include "glm/geometric.hpp"
 #include "glm/trigonometric.hpp"
 #include "input_manager.hpp"
 #include "mesh.hpp"
@@ -58,31 +57,22 @@ int main(int argc, char **argv)
     registry.add_mesh("sphere", {"models/sphere.obj"});
     registry.add_mesh("cube", {"models/cube.obj"});
 
-    const Shader  &shader          = registry.get_shader("default");
-    const Texture &texture         = registry.get_texture("test");
-    const Mesh    &mesh            = registry.get_mesh("sphere");
-
-    // Camera
-    glm::vec3      camera_position = glm::vec3(0.0f, 0.0f, 3.0f);
-    glm::vec3      camera_front    = glm::vec3(0.0f, 0.0f, -1.0f);
-    glm::vec3      camera_up       = glm::vec3(0.0f, 1.0f, 0.0f);
+    const Shader  &shader  = registry.get_shader("default");
+    const Texture &texture = registry.get_texture("test");
+    const Mesh    &mesh    = registry.get_mesh("sphere");
 
     // Position and rotation
     int            width, height;
     SDL_GetWindowSize(window, &width, &height);
+    Camera    camera(width, height);
     glm::mat4 model = glm::mat4(1.0);
-    glm::mat4 view  = glm::mat4(1.0);
-    glm::mat4 proj  = glm::mat4(1.0);
     model           = glm::rotate(model, glm::radians(-55.0f), glm::vec3(1.0, 0.0f, 0.0f));
-    view            = glm::lookAt(camera_position, camera_position + camera_front, camera_up);
-    proj = glm::perspective(glm::radians(45.0f), static_cast<float>(width) / static_cast<float>(height), 0.1f, 100.0f);
 
     glViewport(0, 0, width, height);
     // glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 
     std::vector<SDL_Scancode> keys = {SDL_SCANCODE_W, SDL_SCANCODE_A, SDL_SCANCODE_S, SDL_SCANCODE_D};
     InputManager              input_manager(keys);
-    float                     yaw = 0.0f, pitch = 0.0f;
     u64                       then      = SDL_GetTicks64();
     f64                       elapsed   = 0;
     f64                       msPerTick = 1000.0 / 60.0;
@@ -105,29 +95,30 @@ int main(int argc, char **argv)
 
                 glm::vec2 relative = input_manager.mouse_relative();
                 if (input_manager.is_mouse_moving()) {
-                    yaw += relative.x;
-                    pitch += relative.y;
-                    if (pitch > 89.0f) pitch = 89.0f;
-                    if (pitch < -89.0f) pitch = 89.0f;
+                    glm::vec2 &rotation = camera.angle();
+                    rotation.x += relative.x;
+                    rotation.y += relative.y;
+                    if (rotation.y > 89.0f) rotation.y = 89.0f;
+                    if (rotation.y < -89.0f) rotation.y = 89.0f;
 
                     glm::vec3 direction;
-                    direction.x  = std::cos(glm::radians(yaw)) * std::cos(glm::radians(pitch));
-                    direction.y  = std::sin(glm::radians(pitch));
-                    direction.z  = std::sin(glm::radians(yaw)) * std::cos(glm::radians(pitch));
-                    camera_front = glm::normalize(direction);
+                    direction.x    = std::cos(glm::radians(rotation.x)) * std::cos(glm::radians(rotation.y));
+                    direction.y    = std::sin(glm::radians(rotation.y));
+                    direction.z    = std::sin(glm::radians(rotation.x)) * std::cos(glm::radians(rotation.y));
+                    camera.front() = glm::normalize(direction);
                 }
             }
 
             const float camera_speed = 2.5f * elapsed / 100;
-            if (input_manager.is_held(SDL_SCANCODE_W)) camera_position += camera_speed * camera_front;
-            if (input_manager.is_held(SDL_SCANCODE_S)) camera_position -= camera_speed * camera_front;
+            if (input_manager.is_held(SDL_SCANCODE_W)) camera.position() += camera_speed * camera.front();
+            if (input_manager.is_held(SDL_SCANCODE_S)) camera.position() -= camera_speed * camera.front();
             if (input_manager.is_held(SDL_SCANCODE_A))
-                camera_position -= glm::normalize(glm::cross(camera_front, camera_up)) * camera_speed;
+                camera.position() -= glm::normalize(glm::cross(camera.front(), camera.up())) * camera_speed;
             if (input_manager.is_held(SDL_SCANCODE_D))
-                camera_position += glm::normalize(glm::cross(camera_front, camera_up)) * camera_speed;
+                camera.position() += glm::normalize(glm::cross(camera.front(), camera.up())) * camera_speed;
 
             model = glm::rotate(model, static_cast<float>(elapsed / 100), glm::vec3(0.5, 1.0, 0.0));
-            view  = glm::lookAt(camera_position, camera_position + camera_front, camera_up);
+            camera.update();
 
             elapsed -= 1.0;
         }
@@ -137,8 +128,8 @@ int main(int argc, char **argv)
 
         shader.use();
         shader.set("model", model);
-        shader.set("view", view);
-        shader.set("projection", proj);
+        shader.set("view", camera.camera_view());
+        shader.set("projection", camera.projection_mat());
         texture.use(0);
         mesh.render();
 
