3a6d3473da4f06dc2721e086ca3529e108e8a061
Alexander Cusaac <theradcast16@gmail.com>
2024-12-03 00:11:03 -0500

    feat: Add asset registry for asset management

---
+++ include/asset_registry.hpp
@@ -0,0 +1,37 @@
+#ifndef ASSET_REGISTRY_HPP
+#define ASSET_REGISTRY_HPP
+
+#include <memory>
+#include <string_view>
+#include <unordered_map>
+
+#include "mesh.hpp"
+#include "shader.hpp"
+#include "texture.hpp"
+
+class AssetRegistry
+{
+    std::unordered_map<std::string_view, Shader>  shaders;
+    std::unordered_map<std::string_view, Texture> textures;
+    std::unordered_map<std::string_view, Mesh>    meshes;
+
+  public:
+    static AssetRegistry &get_instance()
+    {
+        static std::unique_ptr<AssetRegistry> registry{nullptr};
+        if (registry == nullptr) {
+            registry = std::make_unique<AssetRegistry>();
+        }
+
+        return *registry;
+    }
+
+    void           add_shader(const std::string_view name, Shader shader);
+    void           add_texture(const std::string_view name, Texture texture);
+    void           add_mesh(const std::string_view name, Mesh mesh);
+    const Shader  &get_shader(const std::string_view name) const;
+    const Texture &get_texture(const std::string_view name) const;
+    const Mesh    &get_mesh(const std::string_view name) const;
+};
+
+#endif
---
+++ src/asset_registry.cpp
@@ -0,0 +1,34 @@
+#include <string_view>
+#include <utility>
+
+#include "asset_registry.hpp"
+
+void AssetRegistry::add_shader(const std::string_view name, Shader shader)
+{
+    if (!shaders.count(name)) shaders[name] = std::move(shader);
+}
+
+void AssetRegistry::add_texture(const std::string_view name, Texture texture)
+{
+    if (!textures.count(name)) textures[name] = std::move(texture);
+}
+
+void AssetRegistry::add_mesh(const std::string_view name, Mesh mesh)
+{
+    if (!meshes.count(name)) meshes[name] = std::move(mesh);
+}
+
+const Shader &AssetRegistry::get_shader(const std::string_view name) const
+{
+    return shaders.at(name);
+}
+
+const Texture &AssetRegistry::get_texture(const std::string_view name) const
+{
+    return textures.at(name);
+}
+
+const Mesh &AssetRegistry::get_mesh(const std::string_view name) const
+{
+    return meshes.at(name);
+}
