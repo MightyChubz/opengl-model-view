e134b8e96bc3216dd8bfe34565b2c9a517cd9da0
Alexander Cusaac <theradcast16@gmail.com>
2024-12-02 22:57:43 -0500

    refactor: Move Vertex into its own header

--- src/main.cpp
+++ src/main.cpp
@@ -32,34 +32,6 @@
 #include "stddefs.hpp"
 #include "texture.hpp"
 
-struct Vertex {
-    glm::vec3 vertice;
-    glm::vec2 texcoord;
-
-    Vertex() = default;
-    Vertex(glm::vec3 vertice, glm::vec2 texcoord) : vertice(vertice), texcoord(texcoord)
-    {
-    }
-
-    bool operator==(const Vertex &v) const
-    {
-        return vertice.x == v.vertice.x && vertice.y == v.vertice.y && vertice.z == v.vertice.z &&
-               texcoord.x == v.texcoord.x && texcoord.y == v.texcoord.y;
-    }
-};
-
-template <> struct std::hash<Vertex> {
-    size_t operator()(const Vertex &v) const noexcept
-    {
-        size_t h1 = std::hash<float>{}(v.vertice.x);
-        size_t h2 = std::hash<float>{}(v.vertice.y);
-        size_t h3 = std::hash<float>{}(v.vertice.z);
-        size_t h4 = std::hash<float>{}(v.texcoord.x);
-        size_t h5 = std::hash<float>{}(v.texcoord.y);
-        return h1 ^ (h2 << 1) ^ (h3 << 2) ^ (h4 << 3) ^ (h5 << 4);
-    }
-};
-
 int main(int argc, char **argv)
 {
     SDL_Init(SDL_INIT_VIDEO);
