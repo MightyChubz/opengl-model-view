34d03ae4d572a1c87ec79e276e820247bbde165f
Alexander Cusaac <theradcast16@gmail.com>
2024-12-02 22:18:33 -0500

    refactor: Make routing array constexpr

--- src/texture.cpp
+++ src/texture.cpp
@@ -58,7 +58,7 @@ Texture::Texture(const std::string_view path)
 
 void Texture::use(size_t slot)
 {
-    std::array texture_slots = {
+    constexpr std::array texture_slots = {
         GL_TEXTURE0,  GL_TEXTURE1,  GL_TEXTURE2,  GL_TEXTURE3,  GL_TEXTURE4,  GL_TEXTURE5,  GL_TEXTURE6,  GL_TEXTURE7,
         GL_TEXTURE8,  GL_TEXTURE9,  GL_TEXTURE10, GL_TEXTURE11, GL_TEXTURE12, GL_TEXTURE13, GL_TEXTURE14, GL_TEXTURE15,
         GL_TEXTURE16, GL_TEXTURE17, GL_TEXTURE18, GL_TEXTURE19, GL_TEXTURE20, GL_TEXTURE21, GL_TEXTURE22, GL_TEXTURE23,
