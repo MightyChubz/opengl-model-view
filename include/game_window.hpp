a65314883eba83d8d310e38f56aa324f02c5ae6d
Alexander Cusaac <theradcast16@gmail.com>
2024-12-12 00:08:44 -0500

    feat: Add game_window for window handling

---
+++ include/game_window.hpp
@@ -0,0 +1,39 @@
+#ifndef GAME_WINDOW_HPP
+#define GAME_WINDOW_HPP
+
+#include "SDL_video.h"
+#include "gl_context.hpp"
+#include <memory>
+#include <string_view>
+class game_window
+{
+    std::shared_ptr<SDL_Window> window{nullptr};
+    gl_context                  context;
+    int                         width;
+    int                         height;
+
+  public:
+    game_window(std::string_view title, int width, int height);
+
+    [[nodiscard]] constexpr SDL_Window *get()
+    {
+        return window.get();
+    }
+
+    [[nodiscard]] constexpr const SDL_GLContext &get_context()
+    {
+        return context.get();
+    }
+
+    [[nodiscard]] constexpr int get_width()
+    {
+        return width;
+    }
+
+    [[nodiscard]] constexpr int get_height()
+    {
+        return height;
+    }
+};
+
+#endif
---
+++ src/game_window.cpp
@@ -0,0 +1,16 @@
+#include "game_window.hpp"
+#include "SDL_video.h"
+#include "gl_context.hpp"
+#include <string_view>
+
+game_window::game_window(std::string_view title, int width, int height)
+    : context(window.get()), width(width), height(height)
+{
+    window.reset(SDL_CreateWindow(title.data(),
+                                  SDL_WINDOWPOS_CENTERED,
+                                  SDL_WINDOWPOS_CENTERED,
+                                  width,
+                                  height,
+                                  SDL_WINDOW_OPENGL),
+                 SDL_DestroyWindow);
+}
