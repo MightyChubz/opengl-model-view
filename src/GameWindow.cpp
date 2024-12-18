441d53698a1ff248dea42d8d4eb65875c548acc2
Alexander Cusaac <theradcast16@gmail.com>
2024-12-18 02:57:08 -0500

    refactor: Move GL attribute setting to GameWindow

--- src/GameWindow.cpp
+++ src/GameWindow.cpp
@@ -8,6 +8,10 @@
 
 GameWindow::GameWindow(std::string_view title, int width, int height) : m_width(width), m_height(height)
 {
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 4);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 1);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+
     m_window.reset(SDL_CreateWindow(title.data(),
                                     SDL_WINDOWPOS_CENTERED,
                                     SDL_WINDOWPOS_CENTERED,
--- src/main.cpp
+++ src/main.cpp
@@ -32,10 +32,6 @@
 int main(int argc, char **argv)
 {
     SdlSubsystem subsystem;
-
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 4);
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 1);
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
     if (!subsystem.IsInitialized()) return 0;
 
     GameWindow window("Testing window", 1280, 720);
