d9ddf7b4a010bc4135f73e516c00b7013c8ad03d
Alexander Cusaac <theradcast16@gmail.com>
2024-12-22 06:40:21 -0500

    feat: Replace mat4 with Transform in Model

--- include/Model.hpp
+++ include/Model.hpp
@@ -2,15 +2,16 @@
 #define MODEL_HPP
 
 #include "Camera.hpp"
+#include "Math.hpp"
 #include "Mesh.hpp"
 #include "Shader.hpp"
 #include "Texture.hpp"
-#include "glm/ext/matrix_float4x4.hpp"
-#include "glm/ext/vector_float3.hpp"
+#include "Transform.hpp"
+#include "glm/detail/qualifier.hpp"
 
 class Model final
 {
-    glm::mat4 m_model{glm::mat4(1.0)};
+    Transform m_transform;
     Mesh      m_mesh;
     Texture   m_texture;
     Shader    m_shader;
@@ -18,7 +19,15 @@ class Model final
   public:
     Model(Mesh &&mesh, Texture &&texture, Shader &&shader);
 
-    void Rotate(const float angle, const glm::vec3 axis);
+    constexpr void Rotate(const float angle, const Vec3 axis)
+    {
+        if (glm::equal(axis, UP_AXIS) == glm::vec<3, bool>(true, true, true))
+            m_transform.GetRotation().y += angle;
+        else if (glm::equal(axis, RIGHT_AXIS) == glm::vec<3, bool>(true, true, true))
+            m_transform.GetRotation().x += angle;
+        else if (glm::equal(axis, FORWARD_AXIS) == glm::vec<3, bool>(true, true, true))
+            m_transform.GetRotation().z += angle;
+    }
 
     void Render(const Camera &camera) const;
 };
--- src/Model.cpp
+++ src/Model.cpp
@@ -5,22 +5,16 @@
 #include "Model.hpp"
 #include "Shader.hpp"
 #include "Texture.hpp"
-#include "glm/ext/vector_float3.hpp"
 
 Model::Model(Mesh &&mesh, Texture &&texture, Shader &&shader)
     : m_mesh(std::move(mesh)), m_texture(std::move(texture)), m_shader(std::move(shader))
 {
 }
 
-void Model::Rotate(const float angle, const glm::vec3 axis)
-{
-    m_model = glm::rotate(m_model, angle, axis);
-}
-
 void Model::Render(const Camera &camera) const
 {
     m_shader.Use();
-    m_shader.Set("model", m_model);
+    m_shader.Set("model", m_transform.GetModel());
     m_shader.Set("view", camera.CameraView());
     m_shader.Set("projection", camera.ProjectionMat());
     m_texture.Use(0);
