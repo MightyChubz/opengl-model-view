c7bc6461bbe2b96d43074f55147fee527ce2f730
Alexander Cusaac <theradcast16@gmail.com>
2024-12-22 07:25:17 -0500

    feat: Overhaul Camera class

--- include/Camera.hpp
+++ include/Camera.hpp
@@ -1,50 +1,47 @@
 #ifndef CAMERA_HPP
 #define CAMERA_HPP
 
-#include "glm/ext/matrix_float4x4.hpp"
-#include "glm/ext/vector_float2.hpp"
-#include "glm/ext/vector_float3.hpp"
+#include "Math.hpp"
+#include "Transform.hpp"
 
 class Camera final
 {
-    glm::mat4 m_projection{glm::mat4(1.0)};
-    glm::mat4 m_view{glm::mat4(1.0)};
-    glm::vec2 m_rotation{0.0F, 0.0F};
-    glm::vec3 m_cameraPosition{glm::vec3(0.0F, 0.0F, 3.0F)};
-    glm::vec3 m_cameraFront{glm::vec3(0.0F, 0.0F, -1.0F)};
-    glm::vec3 m_cameraUp{glm::vec3(0.0F, 1.0F, 0.0F)};
+    Matrix4   m_projection{Matrix4(1.0)};
+    Matrix4   m_view{Matrix4(1.0)};
+    Transform m_transform;
+    Vec3      m_front{FORWARD_AXIS};
 
   public:
     Camera(const int width, const int height);
 
-    [[nodiscard]] constexpr const glm::mat4 &ProjectionMat() const
+    [[nodiscard]] constexpr const Matrix4 &ProjectionMat() const
     {
         return m_projection;
     }
 
-    [[nodiscard]] constexpr const glm::mat4 &CameraView() const
+    [[nodiscard]] constexpr const Matrix4 &CameraView() const
     {
         return m_view;
     }
 
-    constexpr glm::vec3 &Position()
+    constexpr Transform &GetTransform()
     {
-        return m_cameraPosition;
+        return m_transform;
     }
 
-    constexpr glm::vec3 &Front()
+    constexpr Vec3 &GetPosition()
     {
-        return m_cameraFront;
+        return m_transform.GetPosition();
     }
 
-    constexpr glm::vec2 &Angle()
+    constexpr Vec3 &GetRotation()
     {
-        return m_rotation;
+        return m_transform.GetRotation();
     }
 
-    [[nodiscard]] constexpr const glm::vec3 &Up() const
+    constexpr Vec3 &Front()
     {
-        return m_cameraUp;
+        return m_front;
     }
 
     void Update();
--- src/Camera.cpp
+++ src/Camera.cpp
@@ -1,18 +1,13 @@
 #include "Camera.hpp"
-#include "glm/ext/matrix_clip_space.hpp"
-#include "glm/ext/matrix_float4x4.hpp"
-#include "glm/ext/matrix_transform.hpp"
+#include "Math.hpp"
 
 Camera::Camera(const int width, const int height)
 {
-    m_view       = glm::lookAt(m_cameraPosition, m_cameraPosition + m_cameraFront, m_cameraUp);
-    m_projection = glm::perspective(glm::radians(45.0F),
-                                    static_cast<float>(width) / static_cast<float>(height),
-                                    0.1F,
-                                    100.0F);
+    m_view       = LookAt(m_transform.GetPosition(), m_transform.GetPosition() + m_front);
+    m_projection = Perspective(45.0F, static_cast<float>(width) / static_cast<float>(height), 0.1F, 100.0F);
 }
 
 void Camera::Update()
 {
-    m_view = glm::lookAt(m_cameraPosition, m_cameraPosition + m_cameraFront, m_cameraUp);
+    m_view = LookAt(m_transform.GetPosition(), m_transform.GetPosition() + m_front);
 }
