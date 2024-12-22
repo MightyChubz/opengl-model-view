fb739779e75976aadc664c6ec1d5525537503f14
Alexander Cusaac <theradcast16@gmail.com>
2024-12-22 06:57:32 -0500

    feat: Add additional functions for other uses

--- include/Math.hpp
+++ include/Math.hpp
@@ -1,17 +1,21 @@
 #ifndef MATH_HPP
 #define MATH_HPP
 
+#include "glm/detail/qualifier.hpp"
 #include "glm/ext/matrix_float4x4.hpp"
 #include "glm/ext/matrix_transform.hpp"
 #include "glm/ext/vector_float2.hpp"
 #include "glm/ext/vector_float3.hpp"
 #include "glm/ext/vector_float4.hpp"
+#include "glm/gtc/type_ptr.hpp"
 #include "glm/trigonometric.hpp"
 
-using Vec2    = glm::vec2;
-using Vec3    = glm::vec3;
-using Vec4    = glm::vec4;
-using Matrix4 = glm::mat4;
+using Vec2     = glm::vec2;
+using Vec3     = glm::vec3;
+using BoolVec2 = glm::vec<2, bool>;
+using BoolVec3 = glm::vec<3, bool>;
+using Vec4     = glm::vec4;
+using Matrix4  = glm::mat4;
 
 constexpr Vec3 UP_AXIS       = Vec3(0.0, 1.0, 0.0);
 constexpr Vec3 DOWN_AXIS     = Vec3(0.0, -1.0, 0.0);
@@ -33,6 +37,23 @@ constexpr Matrix4 Translate(Matrix4 &mat, Vec3 delta)
 constexpr Matrix4 Scale(Matrix4 &mat, Vec3 scale)
 {
     return glm::scale(mat, scale);
+}
+
+constexpr float *ValuePtr(Matrix4 &mat)
+{
+    return glm::value_ptr(mat);
+}
+
+constexpr bool Vec3Equal(const Vec3 &a, const Vec3 &b)
+{
+    BoolVec3 result = glm::equal(a, b);
+    return result == BoolVec3(true, true, true);
+}
+
+constexpr bool Vec2Equal(const Vec2 &a, const Vec2 &b)
+{
+    BoolVec2 result = glm::equal(a, b);
+    return result == BoolVec2(true, true);
 }
 
 #endif
